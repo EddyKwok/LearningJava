e435dca2660fea679a6e0e4f31bce70cbf9a3077
Eddy Kwok <3ddy.kwok@gmail.com>
2014-12-27 07:19:55 +0700

    Demonstration of the use of Local Classes - Chapter 4 - page 148.

---
+++ MyFistTestingApp/src/Iterator.java
@@ -0,0 +1,5 @@
+public interface Iterator
+{
+	boolean hasMoreElements();
+	Object nextElement();
+}
---
+++ MyFistTestingApp/src/ToDoList.java
@@ -0,0 +1,29 @@
+public class ToDoList
+{
+	private ToDo[] toDoList;
+	private int index = 0;
+	public ToDoList(int size)
+	{
+		toDoList = new ToDo[size];
+	}
+	public Iterator iterator()
+	{
+		class Iter implements Iterator
+		{
+			int index = 0;
+			public boolean hasMoreElements()
+			{
+				return index < toDoList.length;
+			}
+			public Object nextElement()
+			{
+				return toDoList[index++];
+			}
+		}
+		return new Iter();
+	}
+	public void add(ToDo item)
+	{
+		toDoList[index++] = item;
+	}
+}
